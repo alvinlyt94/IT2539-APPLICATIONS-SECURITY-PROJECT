f085e6a42f6fecd04aaf6e0df3a8efafd83bac9e
Alvin Lee <alvinlyt94@hotmail.com>
2017-09-30 03:45:20 +0800

    Add files via upload

---
+++ workspace/MWN/Connector/DBConnector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using MySql.Data.MySqlClient;
+using System.Configuration;
+
+namespace MWN.Connector
+{
+    public class DBConnector
+    {
+        public static MySqlConnection getConnection()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["DBConnectionString"].ConnectionString;
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            return conn;
+        }
+    }
+}
