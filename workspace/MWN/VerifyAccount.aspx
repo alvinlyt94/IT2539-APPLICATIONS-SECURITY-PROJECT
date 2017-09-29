71688a8c8f1389d57485264dd21a56ccf0e4a87c
Alvin Lee <alvinlyt94@hotmail.com>
2017-09-30 03:47:28 +0800

    Add files via upload

---
+++ workspace/MWN/VerifyAccount.aspx
@@ -0,0 +1,191 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="VerifyAccount.aspx.cs" Inherits="MWN.VerifyAccount" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>MWN - My Well-being Network</title>
+</head>
+<body>
+    <!--#include file="Header.html" -->
+
+            <!-- Start Navigation -->
+        <asp:LoginView ID="LoginView1" runat="server">
+            <LoggedInTemplate>
+                <div class="main-container">
+                    <div id="nav-container">
+                        <nav>
+                            <ul class="nav">
+                                <li><a href="HomePage.aspx">Homepage</a></li>
+                                <li><a href="ManageProfile.aspx">Manage Profile</a></li>
+                                <li><a href="#">Tools</a>
+                                    <ul>
+                                        <li><a href="BMICalc.aspx">BMI Calculator</a></li>
+                                        <li><a href="NewsPage.aspx">Health Reader</a></li>
+                                        <li><a href="SendMessage.aspx">Compose Message</a></li>
+                                        <li><a href="ReceiveMessage.aspx">Inbox</a></li>
+                                        <li><a href="Conversation.aspx">Conversation</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Information</a>
+                                    <ul>
+                                        <li><a href="#">Sports</a>
+                                            <ul>
+                                                <li><a href="Sports_Badminton.aspx">Badminton</a></li>
+                                                <li><a href="#">Basketball</a></li>
+                                                <li><a href="#">Bowling</a></li>
+                                                <li><a href="#">Floorball</a></li>
+                                                <li><a href="#">Soccer</a></li>
+                                                <li><a href="#">Softball</a></li>
+                                                <li><a href="#">Swimming</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#">Diets</a>
+                                            <ul>
+                                                <li><a href="#">Breakfast</a></li>
+                                                <li><a href="#">Lunch</a></li>
+                                                <li><a href="#">Tea Break</a></li>
+                                                <li><a href="#">Dinner</a></li>
+                                                <li><a href="#">Supper</a></li>
+                                                <li><a href="#">Midnight Snacks</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#">Food Benefits</a>
+                                            <ul>
+                                                <li><a href="#">Dairy</a></li>
+                                                <li><a href="#">Fruits</a></li>
+                                                <li><a href="#">Mean and beans</a></li>
+                                                <li><a href="#">Oils</a></li>
+                                                <li><a href="#">Vegetables</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li><a href="HealthTopics.aspx">Health Topics</a></li>
+                                <li><a href="FeedbackUs.aspx">Feedback Us</a></li>
+                                <li class="last">
+                                    <asp:LoginStatus ID="LoginStatus1" runat="server" LogoutAction="Redirect" LogoutText="Log off" LogoutPageUrl="HomePage.aspx" />
+                                </li>
+                            </ul>
+                        </nav>
+                        <div class="clear"></div>
+                    </div>
+                </div>
+            </LoggedInTemplate>
+            <AnonymousTemplate>
+                <div class="main-container">
+                    <div id="nav-container">
+                        <nav>
+                            <ul class="nav">
+                                <li><a href="HomePage.aspx">Homepage</a></li>
+                                <li><a href="Login.aspx">Log In</a></li>
+                                <li><a href="Register.aspx">Register</a></li>
+                                <li><a href="#">Tools</a>
+                                    <ul>
+                                        <li><a href="BMICalc.aspx">BMI Calculator</a></li>
+                                        <li><a href="NewsPage.aspx">Health Reader</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Information</a>
+                                    <ul>
+                                        <li><a href="Sports.aspx">Sports</a>
+                                            <ul>
+                                                <li><a href="Sports_Badminton.aspx">Badminton</a></li>
+                                                <li><a href="#">Basketball</a></li>
+                                                <li><a href="#">Bowling</a></li>
+                                                <li><a href="#">Floorball</a></li>
+                                                <li><a href="#">Soccer</a></li>
+                                                <li><a href="#">Softball</a></li>
+                                                <li><a href="#">Swimming</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#">Diets</a>
+                                            <ul>
+                                                <li><a href="#">Breakfast</a></li>
+                                                <li><a href="#">Lunch</a></li>
+                                                <li><a href="#">Tea Break</a></li>
+                                                <li><a href="#">Dinner</a></li>
+                                                <li><a href="#">Supper</a></li>
+                                                <li><a href="#">Midnight Snacks</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#">Food Benefits</a>
+                                            <ul>
+                                                <li><a href="#">Dairy</a></li>
+                                                <li><a href="#">Fruits</a></li>
+                                                <li><a href="#">Mean and beans</a></li>
+                                                <li><a href="#">Oils</a></li>
+                                                <li><a href="#">Vegetables</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li><a href="HealthTopics.aspx">Health Topics</a></li>
+                            </ul>
+                        </nav>
+                        <div class="clear"></div>
+                    </div>
+                </div>
+            </AnonymousTemplate>
+        </asp:LoginView>
+        <!-- End Navigation -->
+
+    <form id="form1" runat="server">
+        <div class="main-container">
+            <div class="container1">
+                <div id="breadcrumb">
+                    <ul>
+                        <li class="first">You Are Here</li>
+                        <li>&#187;</li>
+                        <li><a href="HomePage.aspx">Homepage</a></li>
+                        <li>&#187;</li>
+                        <li><a href="Login.aspx">Login</a></li>
+                        <li>&#187;</li>
+                        <li class="current"><a href="VerifyAccount.aspx">Verify Account</a></li>
+                    </ul>
+                </div>
+                <br />
+                <br />
+                
+                <!-- Advertisement Start -->
+                <center>
+                    <a id="A2" runat="server" onclick="return confirm('MWN will open ad website in a new tab, is that ok?')" onserverclick="ImageAdvertisement_Click">
+                        <asp:Image ID="AdvertisementImage1" runat="server" style="border-style:ridge; border-width:10px; width:800px;"></asp:Image>
+                    </a>
+                </center>
+                <br />
+                <!-- Advertisement End -->
+
+                <a id="A1" runat="server" onServerClick="Keyboard_click" ><img src="images/keyboard.png" align="right" /></a>  
+                <div class="box">
+                    <h3>Verify Account.</h3>
+                    <asp:Label ID="SuccessRegister" runat="server" Font-Bold="True" ForeColor="#009900"></asp:Label>
+                    <b>Enter the given verification code below to verify your account. Or <a href="ResendVerify.aspx">Click Here</a> to resend your verification code.</b>
+                    <br />
+                    <br />
+                    <fieldset style="width: 1000px;">
+                        <h4>Username:</h4>
+                        <asp:TextBox ID="UserName" placeholder="Enter your username" required="required" MaxLength="45" Columns="30" runat="server"></asp:TextBox>
+                        <h4>Verification Code:</h4>
+                        <asp:TextBox ID="VerificationCode" placeholder="Enter your verification code" MaxLength="45" required="required" Columns="30" runat="server"></asp:TextBox>
+
+                        <asp:Label ID="VerificationCodeValidateLabel" runat="server" Font-Bold="True" Font-Size="Medium" ForeColor="#FF3300"></asp:Label>
+
+                        <br />
+                        <br />
+
+                        <asp:Button ID="VerifyButton" runat="server" OnClick="VerifyButton_Click" Text="Verify" />
+
+                    </fieldset>
+                    <br />
+                    <p><a href="Register.aspx">Register</a> if you don't have an account.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="main-container">
+        </div>
+    </form>
+    <!--#include file="Footer.html" -->
+</body>
+</html>
---
+++ workspace/MWN/VerifyAccount.aspx.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using MWN.Connector;
+
+namespace MWN
+{
+    public partial class VerifyAccount : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Advertisement start  
+                string ToShowAds = null;
+                string query00 = "SELECT AdsVisible FROM mwn.account WHERE userName=@1";
+                using (MySqlConnection conn = DBConnector.getConnection())
+                {
+                    conn.Open();
+                    using (MySqlCommand command = new MySqlCommand(query00, conn))
+                    {
+                        command.Parameters.Add(new MySqlParameter("1", User.Identity.Name));
+                        MySqlDataReader reader = command.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            ToShowAds = reader["AdsVisible"].ToString();
+                        }
+                    }
+                }
+                if (ToShowAds == "Enabled" || ToShowAds == null)
+                {
+                    string randomAdvertisementIndex = RandomAdvertisement(1, false);
+                    if (randomAdvertisementIndex == "1")
+                    {
+                        AdvertisementImage1.ImageUrl = "images/milo_banner.jpg";
+                    }
+                    else if (randomAdvertisementIndex == "2")
+                    {
+                        AdvertisementImage1.ImageUrl = "images/nike_banner.jpg";
+                    }
+                    else if (randomAdvertisementIndex == "3")
+                    {
+                        AdvertisementImage1.ImageUrl = "images/adidas_banner.jpg";
+                    }
+                    else if (randomAdvertisementIndex == "4")
+                    {
+                        AdvertisementImage1.ImageUrl = "images/gatorade_banner.jpg";
+                    }
+                    else if (randomAdvertisementIndex == "5")
+                    {
+                        AdvertisementImage1.ImageUrl = "images/nb_banner.jpg";
+                    }
+                }
+                else if (ToShowAds == "Disabled")
+                {
+                    AdvertisementImage1.Visible = false;
+                }
+                // Advertisement end
+
+                // Direct user back to homepage if he/she is logged in
+                if (User.Identity.IsAuthenticated)
+                {
+                    Response.Redirect("Homepage.aspx");
+                }
+
+                // Getting display list
+                string toShow = "NotShow";
+                string fromWhere = "";
+                string query = "SELECT fromToList, fromWhere FROM mwn.fromTo WHERE idFromTo = 1";
+                using (MySqlConnection conn = DBConnector.getConnection())
+                {
+                    conn.Open();
+                    using (MySqlCommand command = new MySqlCommand(query, conn))
+                    {
+                        MySqlDataReader reader = command.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            toShow = reader["fromToList"].ToString();
+                            fromWhere = reader["fromWhere"].ToString();
+                        }
+                    }
+                }
+
+                // Show and not to show Successful Register Message
+                if (toShow == "Show")
+                {
+                    if (fromWhere == "ChangedPassword")
+                    {
+                        SuccessRegister.Text = "Successfully Changed Your Password! A Verification Code has been sent to your email. Please re-verify your account. ";
+                        // Setting back to not show
+                        string query2 = "UPDATE mwn.fromTo SET fromToList=@1 WHERE idFromTo = 1";
+                        using (MySqlConnection conn = DBConnector.getConnection())
+                        {
+                            conn.Open();
+                            using (MySqlCommand command = new MySqlCommand(query2, conn))
+                            {
+                                command.Parameters.Add(new MySqlParameter("1", "NotShow"));
+                                command.ExecuteNonQuery();
+                            }
+                        }
+                    }
+                    else if (fromWhere == "RegisterAccount")
+                    {
+                        SuccessRegister.Text = "Successfully Registered Your Account! A Verification Code has been sent to your email. ";
+                        // Setting back to not show
+                        string query2 = "UPDATE mwn.fromTo SET fromToList=@1 WHERE idFromTo = 1";
+                        using (MySqlConnection conn = DBConnector.getConnection())
+                        {
+                            conn.Open();
+                            using (MySqlCommand command = new MySqlCommand(query2, conn))
+                            {
+                                command.Parameters.Add(new MySqlParameter("1", "NotShow"));
+                                command.ExecuteNonQuery();
+                            }
+                        }
+                    }
+                    else if (fromWhere == "ResendVerify")
+                    {
+                        SuccessRegister.Text = "A New Generated Verification Code has been sent to your email. ";
+                        // Setting back to not show
+                        string query2 = "UPDATE mwn.fromTo SET fromToList=@1 WHERE idFromTo = 1";
+                        using (MySqlConnection conn = DBConnector.getConnection())
+                        {
+                            conn.Open();
+                            using (MySqlCommand command = new MySqlCommand(query2, conn))
+                            {
+                                command.Parameters.Add(new MySqlParameter("1", "NotShow"));
+                                command.ExecuteNonQuery();
+                            }
+                        }
+                    }
+                }
+                else if (toShow == "NotShow")
+                {
+                    SuccessRegister.Text = "";
+                }
+            }
+        }
+
+        protected void VerifyButton_Click(object sender, EventArgs e)
+        {
+            string userName = UserName.Text;
+            string verificationCode = VerificationCode.Text;
+            VerificationCodeValidateLabel.Text = "";
+
+            Boolean toUpdate = false;
+
+            // Comparing VerificationCode according to the username
+            string query = "SELECT * FROM mwn.account WHERE userName = @1 AND verified = @2";
+            using (MySqlConnection conn = DBConnector.getConnection())
+            {
+                conn.Open();
+                using (MySqlCommand command = new MySqlCommand(query, conn))
+                {
+                    command.Parameters.Add(new MySqlParameter("1", userName));
+                    command.Parameters.Add(new MySqlParameter("2", verificationCode));
+                    MySqlDataReader reader = command.ExecuteReader();
+                    // If there is return, means correct VerificationCode
+                    while (reader.Read())
+                    {
+                        toUpdate = true;
+                    }
+                }
+            }
+
+            if (toUpdate == true)
+            {
+                // Setting the verification code of the username to "Verified"
+                string query2 = "UPDATE mwn.account SET verified=@1 WHERE userName=@2";
+                using (MySqlConnection conn = DBConnector.getConnection())
+                {
+                    conn.Open();
+                    using (MySqlCommand command = new MySqlCommand(query2, conn))
+                    {
+                        command.Parameters.Add(new MySqlParameter("1", "Verified"));
+                        command.Parameters.Add(new MySqlParameter("2", userName));
+                        command.ExecuteNonQuery();
+                    }
+                }
+                // Before Redirecting to Login page
+                string query3 = "UPDATE mwn.fromTo SET fromToList=@1, fromWhere=@2 WHERE idFromTo = 1";
+                using (MySqlConnection conn = DBConnector.getConnection())
+                {
+                    conn.Open();
+                    using (MySqlCommand command = new MySqlCommand(query3, conn))
+                    {
+                        command.Parameters.Add(new MySqlParameter("1", "Show"));
+                        command.Parameters.Add(new MySqlParameter("2", "VerifyAccount"));
+                        command.ExecuteNonQuery();
+                    }
+                }
+                // Redirecting to Login page
+                Response.Redirect("Login.aspx");
+            }
+            else
+            {
+                // Checking whether account is already verified or not
+                string verified = "";
+                string query4 = "SELECT verified FROM mwn.account WHERE userName = @1";
+                using (MySqlConnection conn = DBConnector.getConnection())
+                {
+                    conn.Open();
+                    using (MySqlCommand command = new MySqlCommand(query4, conn))
+                    {
+                        command.Parameters.Add(new MySqlParameter("1", userName));
+                        MySqlDataReader reader = command.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            verified = reader["verified"].ToString();
+                        }
+                    }
+                }
+
+                if (verified == "Verified")
+                {
+                    VerificationCodeValidateLabel.Text = "Account already verified.";
+                }
+                else
+                {
+                    VerificationCodeValidateLabel.Text = "Invalid Verification Code. Please re-enter.";
+                }
+            }
+        }
+
+        protected void Keyboard_click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("osk.exe");
+        }
+
+        protected void ImageAdvertisement_Click(object sender, EventArgs e)
+        {
+            // Advertisement start
+            int miloCounter = 0;
+            int nikeCounter = 0;
+            int adidasCounter = 0;
+            int gatoradeCounter = 0;
+            int nbCounter = 0;
+
+            string query = "SELECT miloCounter, nikeCounter, adidasCounter, gatoradeCounter, nbCounter FROM mwn.advertisement WHERE idadvertisement=1";
+            using (MySqlConnection conn = DBConnector.getConnection())
+            {
+                conn.Open();
+                using (MySqlCommand command = new MySqlCommand(query, conn))
+                {
+                    MySqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        miloCounter = Convert.ToInt32(reader["miloCounter"]);
+                        nikeCounter = Convert.ToInt32(reader["nikeCounter"]);
+                        adidasCounter = Convert.ToInt32(reader["adidasCounter"]);
+                        gatoradeCounter = Convert.ToInt32(reader["gatoradeCounter"]);
+                        nbCounter = Convert.ToInt32(reader["nbCounter"]);
+                    }
+                }
+            }
+
+            if (AdvertisementImage1.ImageUrl == "images/milo_banner.jpg")
+            {
+                miloCounter++;
+
+                System.Diagnostics.Process.Start("http://www.nestle.com.sg/milo/");
+            }
+            else if (AdvertisementImage1.ImageUrl == "images/nike_banner.jpg")
+            {
+                nikeCounter++;
+                System.Diagnostics.Process.Start("http://www.nike.com/sg/en_gb/");
+            }
+            else if (AdvertisementImage1.ImageUrl == "images/adidas_banner.jpg")
+            {
+                adidasCounter++;
+                System.Diagnostics.Process.Start("http://www.adidas.com.sg/");
+            }
+            else if (AdvertisementImage1.ImageUrl == "images/gatorade_banner.jpg")
+            {
+                gatoradeCounter++;
+                System.Diagnostics.Process.Start("http://www.gatorade.com/#Home");
+            }
+            else if (AdvertisementImage1.ImageUrl == "images/nb_banner.jpg")
+            {
+                nbCounter++;
+                System.Diagnostics.Process.Start("http://www.newbalance.com.sg/");
+            }
+
+            if (AdvertisementImage1.ImageUrl == "images/milo_banner.jpg" || AdvertisementImage1.ImageUrl == "images/nike_banner.jpg" || AdvertisementImage1.ImageUrl == "images/adidas_banner.jpg" || AdvertisementImage1.ImageUrl == "images/gatorade_banner.jpg" || AdvertisementImage1.ImageUrl == "images/nb_banner.jpg")
+            {
+                string query2 = "UPDATE mwn.advertisement SET miloCounter=@1, nikeCounter=@2, adidasCounter=@3, gatoradeCounter=@4, nbCounter=@5 WHERE idadvertisement=1";
+                using (MySqlConnection conn = DBConnector.getConnection())
+                {
+                    conn.Open();
+                    using (MySqlCommand command = new MySqlCommand(query2, conn))
+                    {
+                        command.Parameters.Add(new MySqlParameter("1", miloCounter));
+                        command.Parameters.Add(new MySqlParameter("2", nikeCounter));
+                        command.Parameters.Add(new MySqlParameter("3", adidasCounter));
+                        command.Parameters.Add(new MySqlParameter("4", gatoradeCounter));
+                        command.Parameters.Add(new MySqlParameter("5", nbCounter));
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+
+            string randomAdvertisementIndex = RandomAdvertisement(1, false);
+            if (randomAdvertisementIndex == "1")
+            {
+                AdvertisementImage1.ImageUrl = "images/milo_banner.jpg";
+            }
+            else if (randomAdvertisementIndex == "2")
+            {
+                AdvertisementImage1.ImageUrl = "images/nike_banner.jpg";
+            }
+            else if (randomAdvertisementIndex == "3")
+            {
+                AdvertisementImage1.ImageUrl = "images/adidas_banner.jpg";
+            }
+            else if (randomAdvertisementIndex == "4")
+            {
+                AdvertisementImage1.ImageUrl = "images/gatorade_banner.jpg";
+            }
+            else if (randomAdvertisementIndex == "5")
+            {
+                AdvertisementImage1.ImageUrl = "images/nb_banner.jpg";
+            }
+            // Advertisment end
+        }
+
+        private static readonly Random Random = new Random();
+
+        private static string RandomAdvertisement(int randomLength, bool strongPassword)
+        {
+            int seed = Random.Next(1, int.MaxValue);
+            //const string allowedChars = "ABCDEFGHJKLMNOPQRSTUVWXYZ0123456789";
+            const string allowedChars = "12345";
+            const string specialCharacters = @"!#$%&'()*+,-./:;<=>?@[\]_";
+
+            var chars = new char[randomLength];
+            var rd = new Random(seed);
+
+            for (var i = 0; i < randomLength; i++)
+            {
+                // If we are to use special characters
+                if (strongPassword && i % Random.Next(3, randomLength) == 0)
+                {
+                    chars[i] = specialCharacters[rd.Next(0, specialCharacters.Length)];
+                }
+                else
+                {
+                    chars[i] = allowedChars[rd.Next(0, allowedChars.Length)];
+                }
+            }
+
+            return new string(chars);
+        }
+    }
+}
